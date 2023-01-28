6a03008b9af19886527d5019b01ed488b9e28ff1
Carl Walters <55022840+Carl-Walters-Portfolio@users.noreply.github.com>
2023-01-28 20:04:40 +0000

    Comments for all functions
    All functions have been commented under.

--- api.py
+++ api.py
@@ -2,10 +2,21 @@ import os
 import openai
 
 
-
-
 class Api():
+    """
+    Controls openai API Further functions will be built into this to give greater control over parameters
+    """
     def call(self, aiText, userText):
+        """
+        Combine's AI prompt and user text and calls the open API with this
+
+        Args:
+            aiText (Str): text from the Ai prompt in the GUI this text should be at the beginning because the GUI encourages the user to use this part as the prompt
+            userText (Str): Users text from the GUI from the section which is for text that may be for manipulation which is handled by theaiText 
+
+        Returns:
+            _type_: _description_
+        """
         openai.api_key = 'Please create an open AI account and create an API key'
         text=aiText + " " + userText
         response = openai.Completion.create(
@@ -17,7 +28,5 @@ class Api():
         frequency_penalty=0,
         presence_penalty=0
         )
-
-        print(response["choices"][0]["text"])
         
         return response["choices"][0]["text"]
--- app.py
+++ app.py
@@ -38,12 +38,25 @@ import json
 state = True
 
 class WorkerSignals(QObject):
+    """ 
+    defines and emit signals for a worker thread.
+    Args:
+        QObject cointains signals
+    """
     finished = pyqtSignal()
     error = pyqtSignal(tuple)
     result = pyqtSignal(object)
     progress = pyqtSignal(int)
 
 class Worker(QRunnable):
+    """
+    Provides event handling for multiple signals to be handled this class is not 
+    currently being utilised but would be useful to have from the beginning in anticipation 
+    for more complex features.
+
+    Args:
+        QRunnable: helps manage a pool of worker threads 
+    """
     def __init__(self, fn, *args, **kwargs):
         super(Worker, self).__init__()
         self.fn = fn
@@ -51,18 +64,17 @@ class Worker(QRunnable):
         self.kwargs = kwargs
         self.signals = WorkerSignals()
 
-    @pyqtSlot()
+    @pyqtSlot() 
     def run(self):
-        pass    
+        """can be used for multithreading"""   
 
 class MainWindow(QMainWindow):
-
+    """
+    MainWindow contains logic that enables and controls various elements within the GUI 
+    """
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         
-        """change grammar
-        
-        """
         uic.loadUi('GUI/mainwindow.ui', self)
         
         self.setWindowTitle("AI-Enabled-Auto-Correction-of-2000-Words-in-One-Click")
@@ -81,12 +93,16 @@ class MainWindow(QMainWindow):
         self.exportToText.pressed.connect(lambda: self.exportToTextFile())
         
         self.setAiContextBox()
-
+        
+    
     def setAiContextBox(self):
         self.aiPromptBox.setPlainText("Correct the spelling and grammar of the following text.")
 
     
     def toggleWhiteSpace(self):
+        """
+        Set weather the return later presented to the user has white space.
+        """
         if self.RemoveWhiteSpace == False:
             self.RemoveWhiteSpace = True
         elif self.RemoveWhiteSpace == True:
@@ -99,10 +115,14 @@ class MainWindow(QMainWindow):
 
 
     def sumbitText(self):
+        """
+        After the submit button has been pressed userText is assigned the text users input 
+        text and aiPromptText Is set to the Ai prompt text
+        """
         self.userInputText.toPlainText()
-        userText = str(self.userInputText.toPlainText())
-        
         aiPromptText = self.aiPromptBox.toPlainText()
+
+        userText = str(self.userInputText.toPlainText())
         
         correctedText = self.api.call(aiPromptText, userText)
         
@@ -114,6 +134,9 @@ class MainWindow(QMainWindow):
         
 
     def exportToTextFile(self):
+        """
+        The Ai output can be exported to a text document by pressing the export button.
+        """
         fileName = "SavedTextFile/" + str(self.textForFile.toPlainText())
         userText = str(self.userInputText.toPlainText())
         self.files.createTextFile(fileName, userText)
